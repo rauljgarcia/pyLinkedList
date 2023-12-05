1d78e3422888a09c07717073551ec4323e2c325b
Raul Garcia <raulgarcia@raul-2.local>
2023-12-05 09:26:57 -0700

    added prepend and pop_first methods

--- py_linked_list.py
+++ py_linked_list.py
@@ -21,24 +21,46 @@ class LinkedList:
         self.length +=1
         return True
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
     def pop(self):
         if self.length == 0:
             return None
         temp = self.head
         pre = self.head
-
         while temp.next:
             pre = temp
             temp = temp.next
         self.tail = pre
         self.tail.next = None
         self.length -= 1
-
         if self.length == 0:
             self.head = None
             self.tail = None
         return temp 
-
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
 
     def print_list(self):
         temp = self.head
@@ -47,10 +69,4 @@ class LinkedList:
             str += (f'{temp.value}->')
             temp = temp.next
         print(f'\t   {str}'+ 'None')
-
-
-
-
-
-
 
