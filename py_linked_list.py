9b8186cb28013bb321ba702a449422036914266c
Raul Garcia <raulgarcia@raul-2.local>
2023-12-05 21:37:31 -0700

    added docstrings

--- py_linked_list.py
+++ py_linked_list.py
@@ -1,9 +1,15 @@
 class Node:
+    '''
+    Create and initialize Node class instance.
+    '''
     def __init__(self, value):
         self.value = value
         self.next = None
 
 class LinkedList:
+    '''
+    Create and initialize LinkedList class instance.
+    '''
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -11,6 +17,14 @@ class LinkedList:
         self.length = 1
     
     def append(self, value):
+        '''
+        Insert data to the end of the linked list.
+        
+        Parameters:
+        value: data type of node
+        
+        Returns: True 
+        '''
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
@@ -22,6 +36,14 @@ class LinkedList:
         return True
 
     def prepend(self, value):
+        '''
+        Insert data to the beginning of the linked list.
+
+        Parameters:
+        value: data type of node
+        
+        Returns: True 
+        '''
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
@@ -30,25 +52,12 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
         self.length += 1
-        return True
-    
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        pre = self.head
-        while temp.next:
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        return temp 
+        return True 
     
     def pop_first(self):
+        '''
+        Remove the first node and return the node's data.
+        '''
         if self.length == 0:
             return None
         temp = self.head
@@ -62,11 +71,32 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def pop(self):
+        '''
+        Remove the last node and return the node's data.
+        '''
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
     def print_list(self):
+        '''
+        This method prints every node's data.
+        '''
         temp = self.head
         str = ''
         while temp is not None:
             str += (f'{temp.value}->')
             temp = temp.next
         print(f'\t   {str}'+ 'None')
-
